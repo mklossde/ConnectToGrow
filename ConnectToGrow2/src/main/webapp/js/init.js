fe4f6657f26a2356901174fe91781e4762ed9f12
Valentin <valentinhafner@gmx.de>
2017-12-12 16:33:24 +0100

    Registry page added

--- ConnectToGrow2/src/main/webapp/js/init.js
+++ ConnectToGrow2/src/main/webapp/js/init.js
@@ -2,6 +2,17 @@
   $(function(){
 
     $('.button-collapse').sideNav();
+    // init selectboxes
+    $('select').material_select();
+    //init datepicker
+    $('.datepicker').pickadate({
+	    selectMonths: true, // Creates a dropdown to control month
+	    selectYears: 15, // Creates a dropdown of 15 years to control year,
+	    today: 'Today',
+	    clear: 'Clear',
+	    close: 'Ok',
+	    closeOnSelect: false // Close upon selecting a date,
+	  });
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
